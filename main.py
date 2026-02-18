8535eb465256f2be791db01c51d56d0310243bad
rue_bee_mae <rubymdecker01@gmail.com>
2026-02-17 20:40:12 -0600

    add comments to main.py

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 def main():
+    # initialize pygame
     pygame.init()
 
+    # setup pygame variables
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # print game start message
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # gameloop
     while True:
-        log_state()
+        log_state()  # log gamestate to jsonl file
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  # quit when user hits the quit button on pygame window
                 return
 
             screen.fill("black")
-            pygame.display.flip()
 
-            dt = clock.tick(60) / 1000
+            pygame.display.flip()  # draw output to screen
+            dt = clock.tick(60) / 1000  # advance delta time
 
 
 if __name__ == "__main__":
